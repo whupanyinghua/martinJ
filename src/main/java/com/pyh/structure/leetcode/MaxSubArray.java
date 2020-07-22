1bc46b545f24329f4c02226d3db46e4f9a2f9fd6
whupanyinghua <whumartine@gmail.com>
2020-07-22 19:45:43 +0800

    代码更新

--- src/main/java/com/pyh/structure/leetcode/MaxSubArray.java
+++ src/main/java/com/pyh/structure/leetcode/MaxSubArray.java
@@ -82,7 +82,7 @@ public class MaxSubArray {
      * 当 dp[i - 1] ≤0 时：执行 dp[i] = nums[i]；
      * 初始状态： dp[0] = nums[0]，即以 nums[0] 结尾的连续子数组最大和为 nums[0]。
      *
-     * 返回值： 返回 dpdp 列表中的最大值，代表全局最大值。
+     * 返回值： 返回 dp 列表中的最大值，代表全局最大值。
      *
      * @param nums
      * @return
