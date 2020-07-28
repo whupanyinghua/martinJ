2c7adbd9f1cb3a52c5adc828e746e8e096ffbca2
whupanyinghua <whumartine@gmail.com>
2020-07-28 12:21:54 +0800

    leetcode：104. 二叉树的最大深度

---
+++ src/main/java/com/pyh/structure/leetcode/MaxDepth.java
@@ -0,0 +1,22 @@
+package com.pyh.structure.leetcode;
+
+/**
+ * 类MaxDepth的实现描述：
+ * 给定一个二叉树，找出其最大深度。
+ *
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+ *
+ * @author panyinghua 2020-7-28 10:55
+ */
+public class MaxDepth {
+
+    public int maxDepth(TreeNode root) {
+        if(null == root) return 0; // 若当前节点为空，则高度是0
+        // 递归左子树的高度、右子树的高度，返回中间较大的值然后+1即为当前树的最大高度
+        return Math.max(maxDepth(root.leftNode), maxDepth(root.rightNode))+1;
+    }
+}
