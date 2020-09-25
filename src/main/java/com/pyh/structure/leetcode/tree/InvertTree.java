6f3ecba721f2b2176537466b49838976439ef8d9
whupanyinghua <whumartine@gmail.com>
2020-09-25 18:06:45 +0800

    添加注释

--- src/main/java/com/pyh/structure/leetcode/tree/InvertTree.java
+++ src/main/java/com/pyh/structure/leetcode/tree/InvertTree.java
@@ -3,7 +3,29 @@ package com.pyh.structure.leetcode.tree;
 import com.pyh.structure.leetcode.TreeNode;
 
 /**
- * 类InvertTree的实现描述：@TODO
+ * 类InvertTree的实现描述：
+ * 翻转一棵二叉树。
+ *
+ * 示例：
+ *
+ * 输入：
+ *
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * 输出：
+ *
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/invert-binary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  *
  * @author panyinghua 2020-9-23 19:23
  */
